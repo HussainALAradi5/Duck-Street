c0c87111c2073c03acf8fe2f66a9f8011015fe64
hussain <hussainaradi.ha@gmail.com>
2024-05-07 10:14:45 +0300

    use dom to query the rows and the columns

--- script.js
+++ script.js
@@ -2,5 +2,12 @@
 const body = document.body
 const game = document.querySelector('.game')
 const rows = game.children
+const ocean = game.querySelector('#ocean')
+const firstRow = body.querySelector('#firstRow')
+const secondRow = body.querySelector('#secondRow')
+const thirdRow = body.querySelector('#thirdRow')
+const fourthRow = body.querySelector('#fourthRow')
+const fifthRow = body.querySelector('#fifthRow')
+let currentLocation = firstRow.querySelector('#r1C1')
 /* functions */
 /* eventLisnter */
